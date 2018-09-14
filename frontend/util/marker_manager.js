7345f22d7de6526fde85f9ce5db87de1b31cb359
Blake Zhai <qz58@duke.edu>
2018-09-13 21:06:14 -0400

    update markers on the map

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -5,7 +5,21 @@ class MarkerManager {
   }
 
   updateMarkers(houses) {
-    console.log('time to update');
+    const housesObj = {};
+    houses.forEach(house => housesObj[house.id] = house);
+
+    houses.filter(house=> !this.markers[house.id]).forEach(newHouse => this.createMarkerFromHouse(newHouse));
+
+  }
+
+  createMarkerFromHouse(house){
+    const houseLatlng = new google.maps.LatLng(house.lat, house.lng);
+    const marker = new google.maps.Marker({
+      position: houseLatlng,
+      map: this.map,
+      houseId: house.id
+    });
+    this.markers[marker.houseId] = marker;
   }
   //...
 }
